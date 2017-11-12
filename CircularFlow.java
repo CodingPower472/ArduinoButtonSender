93ba9045302dddc3a20036e77d646dfaf6feac70
Luke Bousfield <tech.master.champ@gmail.com>
2017-11-12 16:12:57 -0700

    Added pingpong

---
+++ CircularFlow.java
@@ -0,0 +1,44 @@
+import arduino.*;
+import java.util.concurrent.TimeUnit;
+import java.io.*;
+import java.util.Scanner;
+
+public class CircularFlow {
+    static String PORT = "";
+    static int BAUD = 9600;
+    
+    private static String getPort(String fileName) {
+        File f = new File(fileName);
+        String port = null;
+        try {
+            Scanner sc = new Scanner(f);
+            port = sc.nextLine();
+        } catch (Throwable err) {
+            System.out.println(err);
+        }
+        return port;
+    }
+    
+    public static void main(String[] args) {
+        // When new byte recieved, send byte to Arduino
+        System.out.println("Circular flow program initiated");
+        PORT = getPort("port.txt");
+        Arduino arduino = new Arduino(PORT, BAUD);
+        while (true) {
+            String input = arduino.serialRead(1);
+            if (input.equals("a")) {
+                try {
+                    TimeUnit.SECONDS.sleep(5);
+                    arduino.serialWrite("b");
+                } catch (Throwable err) {
+                    System.out.println(err);
+                }
+            }
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (Throwable err) {
+                System.out.println(err);
+            }
+        }
+    }
+}
